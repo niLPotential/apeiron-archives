a22fbebb6db1aca1b76c3d61969e4ed286dfff91
niLPotential <niLPotential@proton.me>
2025-05-19 20:43:24 +0900

    Testing...

--- src/App.tsx
+++ src/App.tsx
@@ -5,85 +5,98 @@ import { Combobox, createListCollection } from "./components/ui/combobox.tsx";
 import { IconButton } from "./components/ui/icon-button.tsx";
 import { TagsInput } from "./components/ui/tags-input.tsx";
 import {
-  isVersion,
-  isVersionName,
+  // isVersion,
+  // isVersionName,
   versionNames,
   versions as versionsList,
 } from "./data/versions.ts";
-import { isSize } from "./data/wallpapers.ts";
+// import { isSize } from "./data/wallpapers.ts";
 
-const initialVersionCollection = createListCollection({
+interface Item {
+  label: string;
+  value: string;
+  type: string;
+}
+
+const initialCollection = createListCollection<Item>({
   "items": versionsList.map((version) => {
     return {
-      "label": `${version}: ${versionNames[version]}`,
-      "value": version,
+      label: `${version}: ${versionNames[version]}`,
+      value: version,
+      type: "version",
     };
   }),
+  "groupBy": (item) => item.type,
 });
 
 function App() {
-  const [versions, setVersions] = useState(initialVersionCollection);
+  const [collection, setCollection] = useState(initialCollection);
 
   function handleInputChange({ inputValue }: Combobox.InputValueChangeDetails) {
-    const filtered = initialVersionCollection.items.filter((item) =>
+    const filtered = initialCollection.items.filter((item) =>
       item.label.includes(inputValue) // TODO: better search method
     );
-    setVersions(createListCollection({ items: filtered }));
+    setCollection(createListCollection({ items: filtered }));
   }
 
   return (
     <>
-      <p>{versions.toString()}</p>
-      <Combobox.Root
-        collection={initialVersionCollection}
-        onInputValueChange={handleInputChange}
-      >
-        <Combobox.Label>검색</Combobox.Label>
-        <Combobox.Control>
-          <TagsInput.Root
-            validate={({ inputValue }: { inputValue: string }) =>
-              isVersion(inputValue) || isSize(inputValue) ||
-              isVersionName(inputValue)}
-          >
-            <TagsInput.Context>
-              {(context) => (
-                <>
-                  <TagsInput.Label>검색</TagsInput.Label>
-                  <TagsInput.Control>
-                    {context.value.map((value, index) => (
-                      <TagsInput.Item key={index} index={index} value={value}>
-                        <TagsInput.ItemPreview>
-                          <TagsInput.ItemText>{value}</TagsInput.ItemText>
-                          <TagsInput.ItemDeleteTrigger asChild>
-                            <IconButton variant="link" size="xs">
-                              <X />
-                            </IconButton>
-                          </TagsInput.ItemDeleteTrigger>
-                        </TagsInput.ItemPreview>
-                      </TagsInput.Item>
+      <p>{collection.toString()}</p>
+      <TagsInput.Root editable={false}>
+        <TagsInput.Context>
+          {(context) => (
+            <>
+              <TagsInput.Control>
+                {context.value.map((value, index) => (
+                  <TagsInput.Item key={index} index={index} value={value}>
+                    <TagsInput.ItemPreview>
+                      <TagsInput.ItemText>{value}</TagsInput.ItemText>
+                      <TagsInput.ItemDeleteTrigger asChild>
+                        <IconButton variant="link" size="xs">
+                          <X />
+                        </IconButton>
+                      </TagsInput.ItemDeleteTrigger>
+                    </TagsInput.ItemPreview>
+                  </TagsInput.Item>
+                ))}
+              </TagsInput.Control>
+              <Combobox.Root
+                collection={collection}
+                inputBehavior="autohighlight"
+                onInputValueChange={handleInputChange}
+                onValueChange={({ items }: { items: Item[] }) => {
+                  items.forEach((item) => context.addValue(item.label));
+                }}
+              >
+                <Combobox.Control>
+                  <TagsInput.Input asChild>
+                    <Combobox.Input />
+                  </TagsInput.Input>
+                </Combobox.Control>
+                <Combobox.Positioner>
+                  <Combobox.Content>
+                    {collection.group().map(([type, group]) => (
+                      <Combobox.ItemGroup key={type}>
+                        <Combobox.ItemGroupLabel>
+                          {type}
+                        </Combobox.ItemGroupLabel>
+                        {group.map((item) => (
+                          <Combobox.Item key={item.value} item={item}>
+                            <Combobox.ItemText>{item.label}</Combobox.ItemText>
+                          </Combobox.Item>
+                        ))}
+                      </Combobox.ItemGroup>
                     ))}
-                    <TagsInput.Input placeholder="Add Framework" />
-                  </TagsInput.Control>
-                  <TagsInput.ClearTrigger asChild>
-                    <Button variant="outline">Clear</Button>
-                  </TagsInput.ClearTrigger>
-                </>
-              )}
-            </TagsInput.Context>
-          </TagsInput.Root>
-        </Combobox.Control>
-        <Combobox.Positioner>
-        <Combobox.Content>
-          <Combobox.ItemGroup>
-            {versions.items.map((item) => (
-              <Combobox.Item key={item.value} item={item}>
-                <Combobox.ItemText>{item.label}</Combobox.ItemText>
-              </Combobox.Item>
-            ))}
-          </Combobox.ItemGroup>
-        </Combobox.Content>
-      </Combobox.Positioner>
-      </Combobox.Root>
+                  </Combobox.Content>
+                </Combobox.Positioner>
+              </Combobox.Root>
+              <TagsInput.ClearTrigger asChild>
+                <Button variant="outline">Clear</Button>
+              </TagsInput.ClearTrigger>
+            </>
+          )}
+        </TagsInput.Context>
+      </TagsInput.Root>
     </>
   );
 }
