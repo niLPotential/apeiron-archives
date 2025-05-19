986f96dd3e3abd27d944d58cb911d1eb035076b6
niLPotential <niLPotential@proton.me>
2025-05-19 05:31:40 +0000

    Seperate version combobox to own file

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
 import { Circle, Monitor, Smartphone } from "lucide-react";
-import { useState } from "react";
 import { useSearchParams } from "react-router";
-import { Combobox, createListCollection } from "./components/ui/combobox.tsx";
 import { ToggleGroup } from "./components/ui/toggle-group.tsx";
-import { type Version, versionNames, versions } from "./data/versions.ts";
-import type { ListCollection } from "@ark-ui/react";
-
-const initialVersionCollection = createListCollection({
-  "items": versions.map((version) => {
-    return {
-      "label": `${version}: ${versionNames[version]}`,
-      "value": version,
-    };
-  }),
-});
+import { VersionCombobox } from "./components/version-combobox.tsx";
 
 function App() {
   const [searchParams] = useSearchParams();
@@ -28,57 +16,9 @@ function App() {
       </div>
       <div>
         <p>{version}</p>
-        <VersionCombobox
-          versionCollection={initialVersionCollection}
-        />
+        <VersionCombobox />
       </div>
     </>
-  );
-}
-
-function VersionCombobox(
-  { versionCollection }: {
-    versionCollection: ListCollection<{ label: string; value: Version }>;
-  },
-) {
-  const [_searchParams, setSearchParams] = useSearchParams();
-  const [versions, setVersions] = useState(versionCollection);
-
-  function handleInputChange({ inputValue }: Combobox.InputValueChangeDetails) {
-    const filtered = versionCollection.items.filter((item) =>
-      item.label.includes(inputValue)
-    );
-    setVersions(createListCollection({ items: filtered }));
-  }
-
-  function handleValueChange({ value }: Combobox.ValueChangeDetails) {
-    setSearchParams({ "version": value });
-  }
-
-  return (
-    <Combobox.Root
-      openOnClick
-      name="version"
-      collection={versionCollection}
-      onInputValueChange={handleInputChange}
-      onValueChange={handleValueChange}
-    >
-      <Combobox.Label>버전</Combobox.Label>
-      <Combobox.Control>
-        <Combobox.Input placehoder="버전을 입력하세요" />
-      </Combobox.Control>
-      <Combobox.Positioner>
-        <Combobox.Content>
-          <Combobox.ItemGroup>
-            {versions.items.map((item) => (
-              <Combobox.Item key={item.value} item={item}>
-                <Combobox.ItemText>{item.label}</Combobox.ItemText>
-              </Combobox.Item>
-            ))}
-          </Combobox.ItemGroup>
-        </Combobox.Content>
-      </Combobox.Positioner>
-    </Combobox.Root>
   );
 }
 
