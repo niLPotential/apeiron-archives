7e94629a7b16619c8031bbd53ba5378127d02b24
niLPotential <niLPotential@proton.me>
2025-05-20 04:05:09 +0000

    Add tests for arcanists

--- src/data/arcanists.ts
+++ src/data/arcanists.ts
@@ -1,6 +1,7 @@
 import type { Version } from "./versions.ts";
 
 export const afflatuses = [
+  "none",
   "beast",
   "mineral",
   "plant",
@@ -13,7 +14,7 @@ export type Afflatus = typeof afflatuses[number];
 export const rarities = [0, 2, 3, 4, 5, 6] as const;
 export type Rarity = typeof rarities[number];
 
-export const damages = ["real", "mental"] as const;
+export const damages = ["none", "real", "mental"] as const;
 export type Damage = typeof damages[number];
 
 export interface Arcanist {
@@ -33,9 +34,21 @@ export async function insertArcanist(kv: Deno.Kv, arcanist: Arcanist) {
     arcanist.version,
     arcanist.name,
   ];
+  const byRarityKey = [
+    "arcanists_by_rarity",
+    arcanist.rarity,
+    arcanist.name,
+  ];
+  const byAfflatusKey = [
+    "arcanists_by_afflatus",
+    arcanist.afflatus,
+    arcanist.name,
+  ];
   await kv.atomic().check({ key: primaryKey, versionstamp: null })
     .set(primaryKey, arcanist)
     .set(byVersionKey, arcanist)
+    .set(byRarityKey, arcanist)
+    .set(byAfflatusKey, arcanist)
     .commit();
 }
 
@@ -46,6 +59,28 @@ export async function getArcanist(kv: Deno.Kv, name: string) {
 export async function getArcanistByVersion(kv: Deno.Kv, version: Version) {
   const iter = kv.list<Arcanist>({
     prefix: ["arcanists_by_version", version],
+  });
+  const arcanists = [];
+  for await (const { value } of iter) {
+    arcanists.push(value);
+  }
+  return arcanists;
+}
+
+export async function getArcanistByRarity(kv: Deno.Kv, rarity: Rarity) {
+  const iter = kv.list<Arcanist>({
+    prefix: ["arcanists_by_rarity", rarity],
+  });
+  const arcanists = [];
+  for await (const { value } of iter) {
+    arcanists.push(value);
+  }
+  return arcanists;
+}
+
+export async function getArcanistByAfflatus(kv: Deno.Kv, afflatus: Afflatus) {
+  const iter = kv.list<Arcanist>({
+    prefix: ["arcanists_by_afflatus", afflatus],
   });
   const arcanists = [];
   for await (const { value } of iter) {
