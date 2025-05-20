95eeaf3866dcc8182b2d269597e60d04a2e10ed5
niLPotential <niLPotential@proton.me>
2025-05-20 01:54:29 +0000

    Add test for wallpapers

---
+++ src/data/wallpapers.test.ts
@@ -0,0 +1,42 @@
+import { assertEquals } from "@std/assert";
+import {
+  getWallpaper,
+  getWallpapersByVersion,
+  insertWallpaper,
+  type Wallpaper,
+} from "./wallpapers.ts";
+
+Deno.test("Wallpapers", async (t) => {
+  const examples: Wallpaper[] = [{
+    id: 6,
+    "pictureUrl":
+      "https://gamecms-res.sl916.com/official_website_resource/50001/4/PICTURE/20230325/Rock'n'roll!-1125x2436_69c37999272740aeb905e5d98d3efd68.jpg",
+    version: "1.0",
+  }, {
+    id: 543,
+    "pictureUrl":
+      "https://gamecms-res.sl916.com/official_website_resource/50001/4/PICTURE/20250507/20_c964b8347e684fe9bf7cc3e364a23185.jpg",
+    version: "1.9", // TODO: Add 2.x versions
+  }];
+  const kv = await Deno.openKv(":memory:");
+
+  await t.step("can insert and get wallpaper", async () => {
+    assertEquals(await getWallpaper(kv, 6), null);
+
+    await insertWallpaper(kv, examples[0]);
+
+    assertEquals(await getWallpaper(kv, 6), examples[0]);
+
+    await insertWallpaper(kv, examples[1]);
+
+    assertEquals(await getWallpaper(kv, 543), examples[1]);
+    assertEquals(await getWallpaper(kv, 6), examples[0]);
+  });
+
+  await t.step("can get wallpapers by version", async () => {
+    assertEquals(await getWallpapersByVersion(kv, "1.0"), [examples[0]]);
+    assertEquals(await getWallpapersByVersion(kv, "1.9"), [examples[1]]);
+  });
+
+  kv.close();
+});
--- src/data/wallpapers.ts
+++ src/data/wallpapers.ts
@@ -1,5 +1,5 @@
-import data from "./data.json" with { type: "json" };
-// import { Version } from "./versions.ts";
+// import data from "./data.json" with { type: "json" };
+import { Version } from "./versions.ts";
 
 type Size = "desktop" | "mobile";
 
@@ -7,14 +7,38 @@ export function isSize(str: string): str is Size {
   return str === "desktop" || str === "mobile";
 }
 
-interface Wallpaper {
+export interface Wallpaper {
   id: number;
   pictureUrl: string;
-  // version: Version;
+  version: Version;
   // characters: string[]; // TODO: Configure characters as constant type
   // size: Size;
 }
 
-export const wallpapers: Wallpaper[] = data.map(({id, pictureUrl}) => {
-  return { id, pictureUrl };
-});
+export async function insertWallpaper(kv: Deno.Kv, wallpaper: Wallpaper) {
+  const primaryKey = ["wallpapers", wallpaper.id];
+  const byVersionKey = [
+    "wallpapers_by_version",
+    wallpaper.version,
+    wallpaper.id,
+  ];
+  await kv.atomic().check({ key: primaryKey, versionstamp: null })
+    .set(primaryKey, wallpaper)
+    .set(byVersionKey, wallpaper)
+    .commit();
+}
+
+export async function getWallpaper(kv: Deno.Kv, id: number) {
+  return (await kv.get(["wallpapers", id])).value as Wallpaper;
+}
+
+export async function getWallpapersByVersion(kv: Deno.Kv, version: Version) {
+  const iter = kv.list<Wallpaper>({
+    prefix: ["wallpapers_by_version", version],
+  });
+  const wallpapers = [];
+  for await (const { value } of iter) {
+    wallpapers.push(value);
+  }
+  return wallpapers;
+}
