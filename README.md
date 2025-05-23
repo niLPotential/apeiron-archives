28513024dfab6d287b309d0d1e52799fcb1e3682
niLPotential <niLPotential@proton.me>
2025-05-23 02:02:43 +0000

    Formatting

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 ## Resources
 
-- [hono-vite-jsx](https://github.com/honojs/examples/tree/main/hono-vite-jsx)
 - [vite backend integration](https://vite.dev/guide/backend-integration.html)
+- [hono-vite-jsx](https://github.com/honojs/examples/tree/main/hono-vite-jsx)
--- src/db/wallpapers.ts
+++ src/db/wallpapers.ts
@@ -1,23 +1,20 @@
 import { kv } from "./index.ts";
 
-const sampleWallpaper = {
-  version: "1.0",
-  "id": 6,
-  "pictureUrl":
-    "https://gamecms-res.sl916.com/official_website_resource/50001/4/PICTURE/20230325/Rock'n'roll!-1125x2436_69c37999272740aeb905e5d98d3efd68.jpg",
-};
-
-await kv.set(
-  ["wallpapers", sampleWallpaper.id],
-  sampleWallpaper,
-);
-
-export async function getWallpaper(id: number) {
-  return (await kv.get(["wallpapers", id])).value as Wallpaper;
-}
-
 export interface Wallpaper {
   id: number;
   pictureUrl: string;
   version: string;
+}
+
+const sampleWallpaper = {
+  version: "1.0",
+  id: 6,
+  pictureUrl:
+    "https://gamecms-res.sl916.com/official_website_resource/50001/4/PICTURE/20230325/Rock'n'roll!-1125x2436_69c37999272740aeb905e5d98d3efd68.jpg",
+};
+
+await kv.set(["wallpapers", sampleWallpaper.id], sampleWallpaper);
+
+export async function getWallpaper(id: number) {
+  return (await kv.get(["wallpapers", id])).value as Wallpaper;
 }
