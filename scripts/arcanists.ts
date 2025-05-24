419246246ae87d681a2664ff1f1f06c2631c0b26
niLPotential <niLPotential@proton.me>
2025-05-24 21:07:07 +0900

    Add arcanists data (1/??)

---
+++ scripts/arcanists.ts
@@ -0,0 +1,154 @@
+import type { Arcanist } from "../src/db/arcanists.ts";
+
+export const allArcanists: Arcanist[] = [
+  {
+    name: "Timekeeper",
+    krName: "버틴",
+    version: "1.0",
+  },
+  {
+    name: "Druvis III",
+    krName: "드루비스",
+    version: "1.0",
+  },
+  {
+    name: "Lilya",
+    krName: "릴리아",
+    version: "1.0",
+  },
+  {
+    name: "A Knight",
+    krName: "A 나이트",
+    version: "1.0",
+  },
+  {
+    name: "Sotheby",
+    krName: "소더비",
+    version: "1.0",
+  },
+  {
+    name: "Regulus",
+    krName: "레굴루스",
+    version: "1.0",
+  },
+  {
+    name: "Centurion",
+    krName: "센츄리온",
+    version: "1.0",
+  },
+  {
+    name: "An-an Lee",
+    krName: "안안 리",
+    version: "1.0",
+  },
+  {
+    name: "Medicine Pocket",
+    krName: "메디슨 포켓",
+    version: "1.0",
+  },
+  {
+    name: "Voyager",
+    krName: "보이저",
+    version: "1.0",
+  },
+  {
+    name: "Eternity",
+    krName: "이터니티",
+    version: "1.0",
+  },
+  {
+    name: "Ms. NewBabel",
+    krName: "뉴바벨",
+    version: "1.0",
+  },
+  {
+    name: "Melania",
+    krName: "멜라니아",
+    version: "1.1",
+  },
+  {
+    name: "Pickles",
+    krName: "피클즈",
+    version: "1.1",
+  },
+  {
+    name: "Tooth Fairy",
+    krName: "투스 페어리",
+    version: "1.2",
+  },
+  {
+    name: "Jessica",
+    krName: "제시카",
+    version: "1.2",
+  },
+  {
+    name: "Kaalaa Baunaa",
+    krName: "갈라보나",
+    version: "1.3",
+  },
+  {
+    name: "Shamane",
+    krName: "갈기 모래",
+    version: "1.3",
+  },
+  {
+    name: "Thirty-seven",
+    krName: "37",
+    version: "1.4",
+  },
+  {
+    name: "Six",
+    krName: "6",
+    version: "1.4",
+  },
+  {
+    name: "Spathodea",
+    krName: "스파토데아",
+    version: "1.5",
+  },
+  {
+    name: "Ezra Theodore",
+    krName: "에즈라",
+    version: "1.5",
+  },
+  {
+    name: "Jiu Niangzi",
+    krName: "곡랑",
+    version: "1.6",
+  },
+  {
+    name: "Getian",
+    krName: "갈천",
+    version: "1.6",
+  },
+  {
+    name: "Isolde",
+    krName: "이졸데",
+    version: "1.7",
+  },
+  {
+    name: "Marcus",
+    krName: "마커스",
+    version: "1.7",
+  },
+  {
+    name: "Vila",
+    krName: "빌라",
+    version: "1.8",
+  },
+  {
+    name: "Windsong",
+    krName: "윈드송",
+    version: "1.8",
+  },
+  {
+    name: "Lucy",
+    krName: "루시",
+    version: "1.9",
+  },
+  {
+    name: "Kakania",
+    krName: "카카니아",
+    version: "1.9",
+  },
+] as const;
