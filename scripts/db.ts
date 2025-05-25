b4c2ba3885abd3470283bbdaa088b00f7b80e0e0
niLPotential <niLPotential@proton.me>
2025-05-25 14:37:36 +0900

    Inserting wallpaper data

--- scripts/db.ts
+++ scripts/db.ts
@@ -1,5 +1,12 @@
 import "jsr:@std/dotenv/load";
+import { insertWallpaper, type Wallpaper } from "../src/db/wallpapers.ts";
 
 export const kv = await Deno.openKv(
   "https://api.deno.com/databases/5d7523de-bbac-4268-9be8-83ae501789ec/connect",
 );
+
+const wallpapers: Wallpaper[] = [];
+
+for (const wallpaper of wallpapers) {
+  await insertWallpaper(kv, wallpaper);
+}
