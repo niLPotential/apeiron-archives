65dfc46b24a85e2eb6442174c97af2a54dc3c6d0
niLPotential <niLPotential@proton.me>
2025-05-26 18:54:13 +0900

    Setup imagekit

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,1 +1,9 @@
+import ImageKit from "imagekit";
+
+export const imagekit = new ImageKit({
+  publicKey: "public_56NZQIPYQ7+R8+dnXjwBbFV90XM=",
+  privateKey: Deno.env.get("IMAGEKIT_PRIVATE_KEY")!,
+  urlEndpoint: " https://ik.imagekit.io/apeironarchives/",
+});
+
 export const kv = await Deno.openKv();
