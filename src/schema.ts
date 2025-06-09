558465f703a2bc13b534999bc1aeb58f38be9639
niLPotential <niLPotential@proton.me>
2025-06-09 06:44:34 +0000

    Simplify table name

--- src/schema.ts
+++ src/schema.ts
@@ -1,9 +1,9 @@
 import { char, pgTable, varchar } from "drizzle-orm/pg-core";
 
-export const versionsTable = pgTable("versions", {
+export const versions = pgTable("versions", {
   id: char({ length: 3 }).primaryKey(),
   en: varchar({ length: 31 }).notNull(),
   kr: varchar({ length: 31 }).notNull(),
 });
 
-export type SelectVersion = typeof versionsTable.$inferSelect;
+export type SelectVersion = typeof versions.$inferSelect;
