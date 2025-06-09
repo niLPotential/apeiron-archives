954e5d293294276b57707388583b7785a21ef82d
niLPotential <niLPotential@proton.me>
2025-06-09 06:44:59 +0000

    Select version data by id or random

--- src/routes/wallpapers/index.tsx
+++ src/routes/wallpapers/index.tsx
@@ -1,12 +1,23 @@
 import { Hono } from "@hono/hono";
 
-import { sql } from "../../db.ts";
+import { db } from "../../db.ts";
+import { versions } from "../../schema.ts";
+import { eq, sql } from "drizzle-orm";
 
 const app = new Hono();
 
-app.get("/", async (c) => {
-  const list = await sql`SELECT * from versions`;
-  return c.json(JSON.stringify(list));
+app.get("/", async(c) => {
+  const data = await db.select().from(versions).orderBy(sql`RANDOM()`).limit(1);
+  return c.json(data);
+})
+
+app.get("/:id", async (c) => {
+  const id = c.req.query("id")!;
+  const data = await db.select().from(versions).where(eq(versions.id, id));
+  if (data.length === 0) {
+    return c.redirect("/");
+  }
+  return c.json(data);
 });
 
 export default app;
