7db9792e0213946526d3176893a6212bc26fdfb0
niLPotential <niLPotential@proton.me>
2025-06-12 04:38:02 +0000

    Add basic styling

--- server.tsx
+++ server.tsx
@@ -1,4 +1,5 @@
 import { Hono } from "@hono/hono";
+import { Style } from "@hono/hono/css";
 import { jsxRenderer } from "@hono/hono/jsx-renderer";
 
 import wallpapers from "./src/routes/wallpapers.tsx";
@@ -10,6 +11,7 @@ app.use(jsxRenderer(({ children }) => (
     <head>
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <Style />
       <title>Apeiron Archives</title>
     </head>
     <body>
--- src/components/wallpaper.tsx
+++ src/components/wallpaper.tsx
@@ -1,3 +1,5 @@
+import { css } from "@hono/hono/css";
+
 import { imagekit, WallpaperData } from "../db.ts";
 
 export function WallpaperImage({ id }: WallpaperData) {
@@ -6,8 +8,13 @@ export function WallpaperImage({ id }: WallpaperData) {
 }
 
 export function WallpapersList({ list }: { list: WallpaperData[] }) {
+  const wallpaperListClass = css`
+    display: flex;
+    flex-direction: column;
+  `;
+
   return (
-    <ul>
+    <ul class={wallpaperListClass}>
       {list.map((wallpaper) => (
         <li key={wallpaper.id}>
           <WallpaperImage {...wallpaper} />
