f2d27a23f9c0f2502b92189ddf6232ecc0c08f58
niLPotential <niLPotential@proton.me>
2025-06-27 02:06:21 +0000

    Attempting to build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { adapter } from "@domcojs/deno";
 import { domco } from "domco";
+import deno from "@deno/vite-plugin";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [domco({
-    adapter: adapter(),
-  })],
+  plugins: [
+    deno(),
+    domco({
+      adapter: adapter(),
+    }),
+  ],
 });
