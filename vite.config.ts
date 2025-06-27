bcf908a4babd409d8c27cc42ece21d7520aae6c2
niLPotential <niLPotential@proton.me>
2025-06-27 06:27:41 +0000

    Remove domco adapter

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,10 @@
-import { adapter } from "@domcojs/deno";
 import { domco } from "domco";
 import deno from "@deno/vite-plugin";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
+    domco(),
     deno(),
-    domco({
-      adapter: adapter(),
-    }),
   ],
 });
