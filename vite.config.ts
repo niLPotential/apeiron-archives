8eb5a6aa73223ddb3de8642c322343c02f71b4f0
niLPotential <niLPotential@proton.me>
2025-07-02 01:08:14 +0000

    Add domco deno adapter

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { domco } from "domco";
+import { adapter } from "@domcojs/deno";
 import deno from "@deno/vite-plugin";
 import unocss from "unocss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
-    domco(),
+    domco({ adapter: adapter() }),
     deno(),
     unocss(),
   ],
