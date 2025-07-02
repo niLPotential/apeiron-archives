59363f7a45d36e687529ff60317103b50783ecc1
niLPotential <niLPotential@proton.me>
2025-07-02 00:56:02 +0000

    Import client scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { PropsWithChildren } from "@hono/hono/jsx";
 
-import { tags } from "client:script";
+import { src } from "client:script";
 
 export default function Layout({ children }: PropsWithChildren) {
   return (
@@ -8,7 +8,8 @@ export default function Layout({ children }: PropsWithChildren) {
       <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        {tags}
+        {src.module.map((v) => <script key={v} type="module" src={v} />)}
+        {src.style.map((v) => <link key={v} rel="stylesheet" href={v} />)}
         <title>Domco</title>
       </head>
       <body>
