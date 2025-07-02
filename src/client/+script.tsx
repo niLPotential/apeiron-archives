ba9360f3aaaeacc3529943c9263f5cab8391e6c6
niLPotential <niLPotential@proton.me>
2025-07-02 23:56:57 +0000

    deno fmt

--- src/client/+script.tsx
+++ src/client/+script.tsx
@@ -2,7 +2,7 @@ import "virtual:uno.css";
 import "htmx.org";
 import Alpine from "alpinejs";
 // @ts-ignore 7017
-globalThis.htmx = require('htmx.org');
+globalThis.htmx = require("htmx.org");
 // @ts-ignore 7017
-globalThis.Alpine = Alpine; 
+globalThis.Alpine = Alpine;
 Alpine.start();
