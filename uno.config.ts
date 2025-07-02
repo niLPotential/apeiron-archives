7782e12c3864b1843f609c89615d2b9f9fc2a6c4
niLPotential <niLPotential@proton.me>
2025-07-02 00:08:17 +0000

    Configure unocss

--- uno.config.ts
+++ uno.config.ts
@@ -1,3 +1,8 @@
-import { defineConfig } from "unocss";
+import { defineConfig, presetMini } from "unocss";
 
-export default defineConfig({});
+export default defineConfig({
+  presets: [presetMini()],
+  content: {
+    filesystem: ["**/*.tsx"],
+  },
+});
