cce81556a99db2aa0b71d724205dc91d9c0213d0
niLPotential <niLPotential@proton.me>
2025-07-02 01:13:17 +0000

    Serve static files through hono

--- src/server/+app.tsx
+++ src/server/+app.tsx
@@ -1,5 +1,6 @@
 import { Hono } from "@hono/hono";
 import { jsxRenderer } from "@hono/hono/jsx-renderer";
+import { serveStatic } from "@hono/hono/deno";
 
 import wallpapers from "./wallpapers.tsx";
 import Layout from "../app/layout.tsx";
@@ -11,6 +12,8 @@ app.use(jsxRenderer(({ children }) => (
     {children}
   </Layout>
 )));
+
+app.use("/_immutable/*", serveStatic({root: "./dist/client/"}))
 
 app.get("/", (c) => c.redirect("/wallpapers"));
 app.route("/wallpapers", wallpapers);
