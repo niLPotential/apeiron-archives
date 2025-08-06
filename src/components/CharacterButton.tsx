74fa869e7088f39b760351b66d85adc75b04d2ca
niLPotential <niLPotential@proton.me>
2025-08-06 05:32:32 +0000

    Disable avatar

--- src/components/CharacterButton.tsx
+++ src/components/CharacterButton.tsx
@@ -1,12 +1,12 @@
-import { imagekit } from "../server/db.ts";
+// import { imagekit } from "../server/db.ts";
 
 export default function CharacterButton(
   { id, name }: { id: string; name: string },
 ) {
-  const src = imagekit.url({ signed: true, path: `./avatar/${id}.jpg` });
+  // const src = imagekit.url({ signed: true, path: `./avatar/${id}.jpg` });
   return (
     <a href={`/wallpapers/characters/${id}`}>
-      <img src={src} alt={id.at(0)} />
+      {/* <img src={src} alt={id.at(0)} /> */}
       <span>{name}</span>
     </a>
   );
