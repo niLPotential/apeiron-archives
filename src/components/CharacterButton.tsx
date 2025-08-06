721bb162d68ed305f6bf8676449fb2682a88305d
niLPotential <niLPotential@proton.me>
2025-08-06 23:44:06 +0000

    Style character list

--- src/components/CharacterButton.tsx
+++ src/components/CharacterButton.tsx
@@ -5,7 +5,7 @@ export default function CharacterButton(
 ) {
   // const src = imagekit.url({ signed: true, path: `./avatar/${id}.jpg` });
   return (
-    <a href={`/wallpapers/characters/${id}`}>
+    <a href={`/wallpapers/characters/${id}`} class="border rounded-full">
       {/* <img src={src} alt={id.at(0)} /> */}
       <span>{name}</span>
     </a>
--- src/components/CharacterList.tsx
+++ src/components/CharacterList.tsx
@@ -3,7 +3,7 @@ import CharacterButton from "./CharacterButton.tsx";
 
 export default function CharacterList({ list }: { list: ArcanistData[] }) {
   return (
-    <ul class="flex flex-col">
+    <ul class="flex flex-col overflow-auto">
       {list.map((character) => (
         <li>
           <CharacterButton id={character.id} name={character.kr} />
