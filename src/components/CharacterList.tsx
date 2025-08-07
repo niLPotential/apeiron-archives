abb030060c1d8832f25f78f08ffd605c4e07d587
niLPotential <niLPotential@proton.me>
2025-08-07 04:58:49 +0000

    Test search

--- src/components/CharacterList.tsx
+++ src/components/CharacterList.tsx
@@ -1,14 +1,34 @@
 import type { ArcanistData } from "../server/db.ts";
-import CharacterButton from "./CharacterButton.tsx";
+// import CharacterButton from "./CharacterButton.tsx";
 
 export default function CharacterList({ list }: { list: ArcanistData[] }) {
   return (
-    <ul class="flex flex-col overflow-auto">
-      {list.map((character) => (
-        <li>
-          <CharacterButton id={character.id} name={character.kr} />
-        </li>
-      ))}
-    </ul>
+    <div
+      x-data={`{
+        query: '',
+        allOptions: ${JSON.stringify(list)},
+        get options() {
+          return this.allOptions.filter((option) => 
+            option.label.toLowerCase().includes(query.toLowerCase())
+          )
+        }
+      }`}
+    >
+      <div class="relative">
+        <input type="search" x-model="query" />
+      </div>
+      <ul class="flex flex-col overflow-auto">
+        <template x-for="item in options" x-bind:key="item.id">
+          <li>
+            <a
+              x-bind:href="'/wallpapers/characters/' + item.id.toString()"
+              class="border rounded-full p-5"
+            >
+              <span x-text="item.kr"></span>
+            </a>
+          </li>
+        </template>
+      </ul>
+    </div>
   );
 }
