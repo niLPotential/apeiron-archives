8f985bd0de382490b34bd5bd69be62d23c62a9cd
niLPotential <niLPotential@proton.me>
2025-08-07 00:02:52 +0000

    Set image height

--- src/app/wallpaper.tsx
+++ src/app/wallpaper.tsx
@@ -1,7 +1,11 @@
 import { imagekit, WallpaperData } from "../server/db.ts";
 
 export function WallpaperImage({ id }: WallpaperData) {
-  const src = imagekit.url({ signed: true, path: `./raw/${id}.jpg` });
+  const src = imagekit.url({
+    signed: true,
+    path: `./raw/${id}.jpg`,
+    transformation: [{ height: 200 }],
+  });
   return <img src={src} alt={`wallpaper-${id}`} />;
 }
 
