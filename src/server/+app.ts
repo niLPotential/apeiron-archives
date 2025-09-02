8adf4300a1350fc1e3235445e2a02688670effa5
nilpotential <nilpotential@proton.me>
2025-09-02 20:37:38 +0900

    Configure preview

--- src/server/+app.ts
+++ src/server/+app.ts
@@ -1,10 +1,11 @@
 import { html } from "client:page";
 import { Hono } from "@hono/hono";
+import { serveStatic } from "@hono/hono/deno";
 
 const app = new Hono();
 
 app.get((c) => c.html(html));
-
 app.post("/clicked", (c) => c.text("Clicked!"));
+app.use("/_immutable/*", serveStatic({ root: "./dist/client/" }));
 
 export default app;
