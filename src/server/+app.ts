b585df7a74ae187fff349d115d59ee1e729eafbd
nilpotential <nilpotential@proton.me>
2025-09-02 20:17:08 +0900

    Configure client

--- src/server/+app.ts
+++ src/server/+app.ts
@@ -1,7 +1,10 @@
+import { html } from "client:page";
 import { Hono } from "@hono/hono";
 
 const app = new Hono();
 
-app.get((c) => c.text("hello"));
+app.get((c) => c.html(html));
+
+app.post("/clicked", (c) => c.text("Clicked!"));
 
 export default app;
