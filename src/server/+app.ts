fd041bee84a01ca6e46f06e4562e67089033b2e7
niLPotential <niLPotential@proton.me>
2025-09-03 08:05:19 +0000

    Add modal

--- src/server/+app.ts
+++ src/server/+app.ts
@@ -1,11 +1,16 @@
-import { html } from "client:page";
+import { html as page } from "client:page";
 import { Hono } from "@hono/hono";
 import { serveStatic } from "@hono/hono/deno";
+import { html } from "@hono/hono/html";
 
 const app = new Hono();
 
-app.get((c) => c.html(html));
+app.get((c) => c.html(page));
 app.post("/clicked", (c) => c.text("Clicked!"));
+app.get("/modal", (c) =>
+  c.html(html`
+    <close-modal></close-modal>
+  `));
 app.use("/_immutable/*", serveStatic({ root: "./dist/client/" }));
 
 export default app;
