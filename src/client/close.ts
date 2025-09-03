22aa1db4b0a67eb12d0d420ee41a86d305d00884
niLPotential <niLPotential@proton.me>
2025-09-03 08:15:55 +0000

    Remove entire component

--- src/client/close.ts
+++ src/client/close.ts
@@ -1,17 +1,12 @@
 import { html, LitElement } from "lit";
 import { customElement } from "lit/decorators.js";
-import { query } from "lit/decorators/query.js";
 
 @customElement("close-modal")
 export class CloseModal extends LitElement {
-  @query("#zoom")
-  // @ts-ignore query
-  _zoom;
-
   override render() {
     return html`
-      <div id="zoom">
-        <button @click="${() => this._zoom.remove()}">Close</button>
+      <div>
+        <button @click="${() => this.remove()}">Close</button>
       </div>
     `;
   }
