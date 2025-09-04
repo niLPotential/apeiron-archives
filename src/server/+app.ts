54f6f42b948bc5790c26049d789c976d5b61a57c
niLPotential <niLPotential@proton.me>
2025-09-04 04:22:44 +0000

    Add sample dialog

--- src/server/+app.ts
+++ src/server/+app.ts
@@ -2,10 +2,13 @@ import { html } from "client:page";
 import { Hono } from "@hono/hono";
 import { serveStatic } from "@hono/hono/deno";
 
+import dialog from "./dialog.tsx";
+
 const app = new Hono();
 
 app.get((c) => c.html(html));
 app.post("/clicked", (c) => c.text("Clicked!"));
+app.route("/dialog", dialog);
 app.use("/_immutable/*", serveStatic({ root: "./dist/client/" }));
 
 export default app;
---
+++ src/server/dialog.tsx
@@ -0,0 +1,13 @@
+import { Hono } from "@hono/hono";
+
+const app = new Hono();
+
+app.get((c) =>
+  c.render(
+    <div x-data x-ref="dialog">
+      <button type="button" x-on:click="$refs.dialog.remove()">Close</button>
+    </div>,
+  )
+);
+
+export default app;
