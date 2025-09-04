14bcae7c23d0682189c0d1dd766887322972159b
niLPotential <niLPotential@proton.me>
2025-09-04 05:04:05 +0000

    Add images route

--- src/server/+app.ts
+++ src/server/+app.ts
@@ -2,9 +2,12 @@ import { html } from "client:page";
 import { Hono } from "@hono/hono";
 import { serveStatic } from "@hono/hono/deno";
 
+import images from "./images.ts";
 import dialog from "./dialog.tsx";
 
 const app = new Hono();
+
+app.route("/images", images);
 
 app.get((c) => c.html(html));
 app.post("/clicked", (c) => c.text("Clicked!"));
