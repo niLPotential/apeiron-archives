ca62048349f5f34f0edeae1355dbff761a15d190
niLPotential <niLPotential@proton.me>
2025-09-04 04:11:39 +0000

    Add alpine counter

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,5 +1,9 @@
 import "virtual:uno.css";
 import htmx from "htmx.org";
+import Alpine from "alpinejs";
 
 // @ts-ignore htmx
 globalThis.htmx = htmx;
+// @ts-ignore alpine
+globalThis.Alpine = Alpine;
+Alpine.start();
