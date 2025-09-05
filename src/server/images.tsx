32df1e52b311bfdebb46a49617570900b9961b52
nilpotential <nilpotential@proton.me>
2025-09-05 23:31:01 +0900

    Handle based on request header

--- src/server/images.tsx
+++ src/server/images.tsx
@@ -1,40 +1,58 @@
 import { Hono } from "@hono/hono";
+import { jsxRenderer } from "@hono/hono/jsx-renderer";
 
 const app = new Hono();
 
-app.get("/random", (c) => {
-  const count = c.req.query("q");
-  return c.html(
-    <div>User requested ${count} images.</div>,
-  );
-});
+app.use(jsxRenderer(({ children }) => (
+  <>
+    <nav hx-boost hx-push-url hx-target="main">
+      <ul>
+        <li>
+          <a href="/images">Home</a>
+        </li>
+        <li>
+          <a href="/images/random">Random</a>
+        </li>
+        <li>
+          <a href="/images/favorites">Favorites</a>
+        </li>
+      </ul>
+    </nav>
+    <main>{children}</main>
+  </>
+)));
 
-app.get("/favorites", (c) => {
-  return c.html(
+app.get("/", (c) => c.render(<div>Home</div>));
+
+app.get("/random", (c) =>
+  c[c.req.header("HX-Request") ? "html" : "render"](
+    <div>User requested random images.</div>,
+  ));
+
+app.get("/favorites", (c) =>
+  c[c.req.header("HX-Request") ? "html" : "render"](
     <div>User requested favorite images.</div>,
-  );
-});
+  ));
 
-app.get("/characters", (c) => {
-  return c.html(
+app.get("/characters", (c) =>
+  c[c.req.header("HX-Request") ? "html" : "render"](
     <div>User requested list of charcters.</div>,
-  );
-});
+  ));
 app.get("/characters/:id", (c) => {
   const id = c.req.param("id");
-  return c.html(
+  return c[c.req.header("HX-Request") ? "html" : "render"](
     <div>User requested images of ${id}.</div>,
   );
 });
 
 app.get("/versions", (c) => {
-  return c.html(
+  return c[c.req.header("HX-Request") ? "html" : "render"](
     <div>User requested list of versions.</div>,
   );
 });
 app.get("/versions/:id", (c) => {
   const id = c.req.param("id");
-  return c.html(
+  return c[c.req.header("HX-Request") ? "html" : "render"](
     <div>User requested images of ${id}.</div>,
   );
 });
