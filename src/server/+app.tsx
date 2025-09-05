b27ad379f6f941051bf24921ae8ae832a9f1c87d
nilpotential <nilpotential@proton.me>
2025-09-05 23:30:37 +0900

    Remove title prop

--- src/server/+app.tsx
+++ src/server/+app.tsx
@@ -5,18 +5,9 @@ import { jsxRenderer } from "@hono/hono/jsx-renderer";
 import { src } from "client:script";
 import images from "./images.tsx";
 
-declare module "@hono/hono" {
-  interface ContextRenderer {
-    (
-      content: string | Promise<string>,
-      props: { title: string },
-    ): Response;
-  }
-}
-
 const app = new Hono();
 
-app.use(jsxRenderer(({ children, title }) => (
+app.use(jsxRenderer(({ children }) => (
   <html lang="en">
     <head>
       <meta charset="UTF-8" />
@@ -26,7 +17,7 @@ app.use(jsxRenderer(({ children, title }) => (
         <link rel="preload" crossorigin="" href={href} key="" />
       ))}
       {src.style.map((href) => <link rel="stylesheet" href={href} key="" />)}
-      <title>{title}</title>
+      <title>Apeiron Archives</title>
     </head>
     <body>
       {children}
@@ -34,35 +25,9 @@ app.use(jsxRenderer(({ children, title }) => (
   </html>
 )));
 
-app.get("/", (c) =>
-  c.render(
-    <main>
-      <button
-        type="button"
-        hx-get="/dialog"
-        hx-target="main"
-        hx-swap="beforeend"
-        hx-push-url
-        class="bg-black text-white"
-      >
-        Open
-      </button>
-    </main>,
-    { title: "main" },
-  ));
-
+app.get("/", (c) => c.redirect("/images"));
 app.route("/images", images);
 
-app.get("/dialog", (c) => {
-  if (!c.req.header("HX-Request")) {
-    return c.redirect("/");
-  }
-  return c.html(
-    <div x-data x-ref="dialog">
-      <button type="button" x-on:click="$refs.dialog.remove()">Close</button>
-    </div>,
-  );
-});
 app.use("/_immutable/*", serveStatic({ root: "./dist/client/" }));
 
 export default app;
