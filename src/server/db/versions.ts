6dc18b6642882a7fd3e6929dab6bc38f79fb1065
niLPotential <niLPotential@proton.me>
2025-09-18 00:45:38 +0000

    Add basic versions data

---
+++ src/server/db/versions.ts
@@ -0,0 +1,83 @@
+export interface VersionData {
+  id: string;
+  en: string;
+  kr: string;
+}
+
+export const Versions: VersionData[] = [{
+  id: "1.0",
+  en: "",
+  kr: "빗속의 재연된 과거",
+}, {
+  id: "1.1",
+  en: "쥘 리메 컵 도난 사건",
+  kr: "The Theft of the Rimet Cup",
+}, {
+  id: "1.2",
+  en: "A Nightmare at Green Lake",
+  kr: "그린 레이크의 악몽",
+}, {
+  id: "1.3",
+  en: "Journey to Mor Pankh",
+  kr: "모르판크로의 여행",
+}, {
+  id: "1.4",
+  en: "The Prisoner in the Cave",
+  kr: "동굴 속의 죄수",
+}, {
+  id: "1.5",
+  en: "Revival! The Uluru Games",
+  kr: "부활! 울루루 대회",
+}, {
+  id: "1.6",
+  en: "Notes on Shuori",
+  kr: "삭일수기",
+}, {
+  id: "1.7",
+  en: "E lucevan le stelle",
+  kr: "별은 빛나건만",
+}, {
+  id: "1.8",
+  en: "Farewell, Rayashki",
+  kr: "안녕, 라야시키",
+}, {
+  id: "1.9",
+  en: "Vereinsamt",
+  kr: "고독의 노래",
+}, {
+  id: "2.0",
+  en: "Floor It! To the Golden City",
+  kr: "질주! 골든 시티로",
+}, {
+  id: "2.1",
+  en: "Route 77: The Haunted Highway",
+  kr: "루트77: 유령의 도로",
+}, {
+  id: "2.2",
+  en: "Tristes Tropiques",
+  kr: "슬픈 열대",
+}, {
+  id: "2.3",
+  en: "Cronicles of Uluru: London Dawning",
+  kr: "울루루 연대기: 런던의 여명",
+}, {
+  id: "2.4",
+  en: "Last Evenings on Earth",
+  kr: "지구에서의 마지막 밤",
+}, {
+  id: "2.5",
+  en: "Showdown in Chinatown",
+  kr: "차이나타운 무비",
+}, {
+  id: "2.6",
+  en: "Folie et Déraison",
+  kr: "광기의 역사",
+}, {
+  id: "2.7",
+  en: "1987 Cosmic Overture",
+  kr: "1987 우주의 서곡",
+}, {
+  id: "2.8",
+  en: "Paradise Regained",
+  kr: "복낙원",
+}] as const;
